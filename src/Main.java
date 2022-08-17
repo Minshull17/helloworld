3795acd581c655e44532a3df6516752d6f162745
Robert Minshull <robminshull126@gmail.com>
2022-08-17 06:14:56 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int loan = 70000;
+        int valuation = 100000;
+        int gearing = valuation * loan;
+        System.out.println(loan);
+        System.out.println(gearing);
+    }
+}
